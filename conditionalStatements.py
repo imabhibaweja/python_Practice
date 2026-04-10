4c61f54a7cc7ed1a90739941107a9cbdfc7c8b95
Abhi Baweja <abaweja001@gmail.com>
2026-04-10 19:35:58 +0530

    match Case added

--- conditionalStatements.py
+++ conditionalStatements.py
@@ -8,3 +8,23 @@ else:
     print("You are minor")
     print("you are not allow to drive")
 print("The End of if-else")
+
+#Match case
+day = "Sunday"
+match day:
+    case 'Monday': 
+        print("It\'s Monday")
+    case 'Tuesday': 
+        print("It\'s Tuesday")
+    case 'Wednesday': 
+        print("It\'s Wednesday")
+    case 'Thursday': 
+        print("It\'s Thursday")
+    case 'Friday': 
+        print("It\'s Friday")
+    case 'Saturday': 
+        print("It\'s Saturday")
+    case 'Sunday': 
+        print("It\'s Sunday")
+    case _:
+        print("Nikal Pahli fursat me!!!")
