29a22a87f63715de6e5a355d478271073fd93ed7
Abhi Baweja <abaweja001@gmail.com>
2026-04-10 17:58:56 +0530

    conditional statements

---
+++ conditionalStatements.py
@@ -0,0 +1,10 @@
+age=12
+if(age>18):
+    print("You are an adult")
+    print("You are allow to drive")
+elif(age==18):
+    print("you need to give a licence test")
+else:
+    print("You are minor")
+    print("you are not allow to drive")
+print("The End of if-else")
