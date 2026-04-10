74fd130446cbe641dbf2c5064e9c50eee48b2daf
Abhi Baweja <abaweja001@gmail.com>
2026-04-10 11:13:31 +0530

    operators file added

---
+++ operators.py
@@ -0,0 +1,44 @@
+a =34
+b=2
+#Arithmetic Operations
+
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a%b=",a%b)
+print("a**b=",a**b)
+print("a//b=",a//b)
+
+#assignment operators 
+a =34
+b=2
+a**=b
+print(a)
+
+a =34
+b=2
+a//=b
+print(a)
+
+a =34
+b=2
+a/=b
+print(a)
+
+#membership operators
+
+fruits={"apple","banana","Mango"}
+print("banana" in fruits)
+print('banana' in fruits)
+print("banana" not in fruits)
+print("orange" in fruits)
+print("Apple" in fruits)
+
+#identity operators
+x=9
+y=9
+z=10
+print(x is  y)
+print(x is z)
+print(x is not z)
