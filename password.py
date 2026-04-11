26b05129c8d45891fd24028b79e2cb9321a397a6
Abhi Baweja <abaweja001@gmail.com>
2026-04-11 12:17:36 +0530

    password in assignment

---
+++ password.py
@@ -0,0 +1,7 @@
+password = ""
+while(password!="123456"):
+    password=input("Enter password:")
+    if(password == "123456"):
+        print("correct password")
+    else:
+        print("Wrong Password!!!")
