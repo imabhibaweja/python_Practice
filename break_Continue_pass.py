624ec9eee1f8d98df725d02004e61d86231aa6e2
Abhi Baweja <abaweja001@gmail.com>
2026-04-11 11:00:54 +0530

    continue and pass added

--- break_Continue_pass.py
+++ break_Continue_pass.py
@@ -1,7 +1,25 @@
+#break
+
 i=1
 while(i<6):
     if(i==3):
         break
     print(i)
     i+=1
-    
+
+#continue
+i=1
+while(i<6):
+    if(i==3):
+        i+=1
+        continue
+    print(i)
+    i+=1
+
+#pass
+i=1
+while(i<6):
+    if(i==3):
+        pass
+    print(i)
+    i+=1
