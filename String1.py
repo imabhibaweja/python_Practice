f1619f51530c1ca9fa5ef74729ed73a1e02b0a09
Abhi Baweja <abaweja001@gmail.com>
2026-04-26 18:34:43 +0530

    find and replace

--- String1.py
+++ String1.py
@@ -6,7 +6,12 @@
 # print(name[:4])
 # print(name[1:])
 # print(len(name))
-name = " Abhi Baweja "
-print(name.strip())
-print(name.lstrip())
-print(name.rstrip())
+
+# name = " Abhi Baweja "
+# print(name.strip())
+# print(name.lstrip())
+# print(name.rstrip())
+
+name = "Baweja and Family"
+print(name.find("and"));
+print(name.replace("and", "with"))
