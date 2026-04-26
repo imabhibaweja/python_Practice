97cce36a72e50dd6f6c5dd154ef048b0674bbde5
Abhi Baweja <abaweja001@gmail.com>
2026-04-26 18:25:01 +0530

    strip function added

--- String1.py
+++ String1.py
@@ -1,8 +1,12 @@
-name="Harry0123456789"
-print(name[0:2])
-print(name[2:-1])
-print(name[0:10:1])
-print(name[0:10:3])
-print(name[:4])
-print(name[1:])
-print(len(name))
+# name="Harry0123456789"
+# print(name[0:2])
+# print(name[2:-1])
+# print(name[0:10:1])
+# print(name[0:10:3])
+# print(name[:4])
+# print(name[1:])
+# print(len(name))
+name = " Abhi Baweja "
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
