1bb57f0562db7e6805cae059127b5bef76228ffb
Abhi Baweja <abaweja001@gmail.com>
2026-05-26 09:40:05 +0530

    lambda function

--- function.py
+++ function.py
@@ -1,6 +1,8 @@
 
 
-def average(a,b,c):
-    avg=(a+b+c)/3
-    print(avg)
-average(2,3,4)
+# def average(a,b,c):
+#     avg=(a+b+c)/3
+#     print(avg)
+# average(2,3,4)
+average = lambda a,b,c: (a+b+c)/3
+print(average(3,4,5))
