6bca0660f99c0ba51109f7b43f39d1c21a9068f4
Abhi Baweja <abaweja001@gmail.com>
2026-05-26 10:00:27 +0530

    testing lambda function

--- function.py
+++ function.py
@@ -4,5 +4,7 @@
 #     avg=(a+b+c)/3
 #     print(avg)
 # average(2,3,4)
-average = lambda a,b,c: (a+b+c)/3
-print(average(3,4,5))
+# average = lambda a,b,c: (a+b+c)/3
+# print(average(3,4,5))
+name= lambda name: print(name)
+name("Abhi")
