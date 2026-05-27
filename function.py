887c249f1c7d9590158d5fd9893362872f82454b
Abhi Baweja <abaweja001@gmail.com>
2026-05-27 10:02:19 +0530

    import module added

--- function.py
+++ function.py
@@ -1,10 +1,14 @@
-
-
 # def average(a,b,c):
 #     avg=(a+b+c)/3
 #     print(avg)
 # average(2,3,4)
 # average = lambda a,b,c: (a+b+c)/3
 # print(average(3,4,5))
-name= lambda name: print(name)
-name("Abhi")
+# name= lambda name: print(name)
+# name("Abhi")
+import arthmeticOperations
+print(arthmeticOperations.sum(10,20))
+print(arthmeticOperations.sub(20,10))
+print(arthmeticOperations.multiple(10,30))
+print(arthmeticOperations.divide(10,20))
+print(arthmeticOperations.reminder(10,21))
