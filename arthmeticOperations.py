f6ccbcaf9679943f312a80e3ceb1a1d5e1502dd7
Abhi Baweja <abaweja001@gmail.com>
2026-05-27 10:01:43 +0530

    module file added

---
+++ arthmeticOperations.py
@@ -0,0 +1,13 @@
+sum = lambda a,b:a+b;
+sub=lambda a,b: a-b
+multiple = lambda a,b: a*b
+def divide(a,b):
+    if(a>b):
+        return a/b
+    else:
+        return b/a
+def reminder (a,b):
+    if(a>b):
+        return a%b
+    else:
+        return b%a
