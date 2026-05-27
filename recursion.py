f192977ef091a43091d332fb0deb1545d3cfbeae
Abhi Baweja <abaweja001@gmail.com>
2026-05-27 09:27:28 +0530

    fibbonic series added

--- recursion.py
+++ recursion.py
@@ -1,5 +1,11 @@
 def fact(a):
     if(a==0 or a==1):
-        return a
+        return 1
     return a*fact(a-1)
+def fibbo(a):
+    if(a==0 or a==1):
+        return a
+    return fibbo(a-1)+fibbo(a-2)
 print(fact(5))
+print(fibbo(5))
+print(fibbo(6))
