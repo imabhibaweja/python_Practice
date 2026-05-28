3fb471eab5a1feac7208185f4134144948f5d79c
Abhi Baweja <abaweja001@gmail.com>
2026-05-28 11:17:20 +0530

    list creation

--- lists.py
+++ lists.py
@@ -1,12 +1,18 @@
-marks = [20,80,40,10,60]
-# mixed = [1, "Abhi Baweja", 8.5, True]
-# print(f"My name is {mixed[1]}, my roll number is {mixed[0]}, my CGPA is {mixed[2]} and its {mixed[3]} that I am passed")
-# print(f"My marks are as follow {marks}")
-marks.reverse()
-print(marks)
-marks.remove(80)
-print(marks)
-print(marks.count(20))
-marks.clear()
-print(marks)
+# marks = [20,80,40,10,60]
+# # mixed = [1, "Abhi Baweja", 8.5, True]
+# # print(f"My name is {mixed[1]}, my roll number is {mixed[0]}, my CGPA is {mixed[2]} and its {mixed[3]} that I am passed")
+# # print(f"My marks are as follow {marks}")
+# marks.reverse()
+# print(marks)
+# marks.remove(80)
+# print(marks)
+# print(marks.count(20))
+# marks.clear()
+# print(marks)
 
+#crate a table of 5 in list
+table =[]
+a = 5
+for i in range (1,11):
+    table.append(a*i)
+print(table)
