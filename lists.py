d862b6d1be275746150f1d56029b0bb9eaf6dbc0
Abhi Baweja <abaweja001@gmail.com>
2026-05-28 10:44:48 +0530

    list explained

---
+++ lists.py
@@ -0,0 +1,4 @@
+marks = [20,80,40,10,60]
+mixed = [1, "Abhi Baweja", 8.5, True]
+print(f"My name is {mixed[1]}, my roll number is {mixed[0]}, my CGPA is {mixed[2]} and its {mixed[3]} that I am passed")
+print(f"My marks are as follow {marks}")
