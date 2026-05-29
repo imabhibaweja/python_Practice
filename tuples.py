4c2b6a090f06f111c61e0f40f22dcbc5f0650760
Abhi Baweja <abaweja001@gmail.com>
2026-05-29 10:57:39 +0530

    tuple unpacking

--- tuples.py
+++ tuples.py
@@ -1,9 +1,14 @@
+# tuple1 = (1,"a", 2.6, True)
+# print(tuple1)
+# print(tuple1[0])
+# print(tuple1[1])
+# print(tuple1[2])
+# print(tuple1[3])
+# tuple2 = (2,) #if we need to create single element tuple we need to add coma(required) at end
+# print(tuple2)
+# print(tuple2[0])
+
+#tuple unpacking
 tuple1 = (1,"a", 2.6, True)
-print(tuple1)
-print(tuple1[0])
-print(tuple1[1])
-print(tuple1[2])
-print(tuple1[3])
-tuple2 = (2,) #if we need to create single element tuple we need to add coma(required) at end
-print(tuple2)
-print(tuple2[0])
+a,b,c,d =tuple1
+print(a,b,c,d)
