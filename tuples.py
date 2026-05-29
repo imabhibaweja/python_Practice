c0592d808d535dcae34728468656d6c9e376daeb
Abhi Baweja <abaweja001@gmail.com>
2026-05-29 10:55:10 +0530

    single-element tuple added

--- tuples.py
+++ tuples.py
@@ -4,3 +4,6 @@ print(tuple1[0])
 print(tuple1[1])
 print(tuple1[2])
 print(tuple1[3])
+tuple2 = (2,) #if we need to create single element tuple we need to add coma(required) at end
+print(tuple2)
+print(tuple2[0])
