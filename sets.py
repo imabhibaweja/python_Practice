a301e98adc1c456d51e4545dd2eb02530d98119b
Abhi Baweja <abaweja001@gmail.com>
2026-05-29 11:45:10 +0530

    sets operations

--- sets.py
+++ sets.py
@@ -5,15 +5,21 @@
 # set2 = {True, 1}
 # print(set2)
 
-set1 = {2, "Abhi", 77.0, True}
-print(set1)
-set1.add(20)
-set1.add(50)
-print(set1)
-set1.remove(20)
-print(set1)
-set1.discard(50)
-set1.discard(234)
-print(set1)
-set1.pop()
-print(set1)
+# set1 = {2, "Abhi", 77.0, True}
+# print(set1)
+# set1.add(20)
+# set1.add(50)
+# print(set1)
+# set1.remove(20)
+# print(set1)
+# set1.discard(50)
+# set1.discard(234)
+# print(set1)
+# set1.pop()
+# print(set1)
+
+a={1,2,3}
+b={3,4,5}
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
