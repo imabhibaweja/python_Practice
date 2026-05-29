f672cbb468fe5c0c360b22684849fd37fccd8c89
Abhi Baweja <abaweja001@gmail.com>
2026-05-29 14:11:39 +0530

    updated again

--- practice.py
+++ practice.py
@@ -1,7 +1,12 @@
-a=(10,20)
-print(a, type(a))
-b=list(a)
-print(b, type(b))
-b[0]=50
-a=tuple(b)
-print(a, type(a))
+# a=(10,20)
+# print(a, type(a))
+# b=list(a)
+# print(b, type(b))
+# b[0]=50
+# a=tuple(b)
+# print(a, type(a))
+numbers = {"Abhi":7410841001, "Rohit":7410841002, "Satyarth":7410841003}
+print(numbers.keys())
+print(numbers.values())
+for keys, values in numbers.items():
+    print(keys, values)
