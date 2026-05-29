84c81cc8ee236fb9ac58b6ea6e23843e9e707e6f
Abhi Baweja <abaweja001@gmail.com>
2026-05-29 11:31:58 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,6 @@
+set1 = {2, "cherry", 2.8, 1, True} 
+print(set1)
+'''# in sets true = 1, false = 0 in python language if {1, True} its output is 1 and 
+if {True, 1} its output is True'''
+set2 = {True, 1}
+print(set2)
