2c9aa269e763cd23b0e33796f2949225ea07f617
Abhi Baweja <abaweja001@gmail.com>
2026-05-29 11:40:33 +0530

    sets method

--- sets.py
+++ sets.py
@@ -1,6 +1,19 @@
-set1 = {2, "cherry", 2.8, 1, True} 
+# set1 = {2, "cherry", 2.8, 1, True} 
+# print(set1)
+# '''# in sets true = 1, false = 0 in python language if {1, True} its output is 1 and 
+# if {True, 1} its output is True'''
+# set2 = {True, 1}
+# print(set2)
+
+set1 = {2, "Abhi", 77.0, True}
 print(set1)
-'''# in sets true = 1, false = 0 in python language if {1, True} its output is 1 and 
-if {True, 1} its output is True'''
-set2 = {True, 1}
-print(set2)
+set1.add(20)
+set1.add(50)
+print(set1)
+set1.remove(20)
+print(set1)
+set1.discard(50)
+set1.discard(234)
+print(set1)
+set1.pop()
+print(set1)
