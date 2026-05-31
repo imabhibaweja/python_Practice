6d4ea1dff1584b6455c9ef1a9a2b780d572620e6
Abhi Baweja <abaweja001@gmail.com>
2026-05-31 10:12:49 +0530

    constructor with default values

---
+++ classAndObject.py
@@ -0,0 +1,32 @@
+
+class Employee:
+    def __init__ (self, name = "Unknown", post = "programmer", salary = 20000):
+        self.name = name
+        self.post = post
+        self.salary = salary
+
+    def get_name(self):
+        return self.name
+    
+    def get_salary(self):
+        return self.salary
+    
+    def get_post(self):
+        return self.post
+    
+a = Employee("Abhi", "CEO", 50000)
+b = Employee("Aman", "Director", 70000)
+print(a.get_name())
+print(a.get_post())
+print(a.get_salary())
+print()
+print(b.get_name())
+print(a.get_post())
+print(a.get_salary())
+    
+
+# a=Employee()
+# a_name = a.get_name()
+# a_salary = a.get_salary()
+# print(a_name)
+# print(a_salary)
