646952d4ce834e41054188bcfcaaae2470209d48
Abhi Baweja <abaweja001@gmail.com>
2026-06-01 10:25:31 +0530

    with default values

--- classAndObject.py
+++ classAndObject.py
@@ -1,22 +1,47 @@
 
 class Employee:
-    def __init__ (self, name = "Unknown", post = "programmer", salary = 20000):
+    # def __init__ (self, name = "Unknown", post = "programmer", salary = 20000):
+    #     self.name = name
+    #     self.post = post
+    #     self.salary = salary
+    def __init__(self, name, post, salary):
         self.name = name
         self.post = post
         self.salary = salary
 
+    # withot default values
+    # def get_name(self):
+    #     self.name = input("Enter name")
+    #     return self.name
+
+    # WITH DEFAULT VALUES
     def get_name(self):
         return self.name
+
+    # withot default values
+
+    # def get_salary(self):
+    #     self.salary = input("Enter salary")
+    #     return self.salary
     
+    #WITH DEFAULT VALUES
     def get_salary(self):
         return self.salary
-    
+
+    #WITHOUT DEFAULT VALUES
+
+    # def get_post(self):
+    #     self.post = input("Enter Post")
+    #     return self.post
+
+    #WITH DEFAULT VALUES
+
     def get_post(self):
         return self.post
     
-a = Employee("Abhi", "CEO", 50000)
+a = Employee("Abhi", "CEO", 50000) # without default values
 b = Employee("Aman", "Director", 70000)
-c = Employee("Harshita")
+# c = Employee() # with default values
 print(a.get_name())
 print(a.get_post())
 print(a.get_salary())
@@ -25,9 +50,9 @@ print(b.get_name())
 print(b.get_post())
 print(b.get_salary())
 print()
-print(c.get_name())
-print(c.get_post())
-print(c.get_salary())
+# print(c.get_name())
+# print(c.get_post())
+# print(c.get_salary())
     
 
 # a=Employee()
