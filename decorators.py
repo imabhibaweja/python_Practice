77207c4ddc1fb6b498fb8bd4aac2267e8fceebcf
Abhi Baweja <abaweja001@gmail.com>
2026-06-28 09:02:22 +0530

    multiple decorators

--- decorators.py
+++ decorators.py
@@ -1,13 +1,41 @@
-#decorator is a function that takes a function, its creat a new function inside its body and then its return the new function.
-def dec(func):
-    def wrap():
-        print("Aman")
-        func()
-        print("Amisha")
-    return wrap
+# #decorator is a function that takes a function, its creat a new function inside its body and then its return the new function.
+# def dec(func):
+#     def wrap():
+#         print("Aman")
+#         func()
+#         print("Amisha")
+#     return wrap
 
-def weds():
-    print("Weds")
+# def weds():
+#     print("Weds")
 
-f = dec(weds)
-f()
+# f = dec(weds)
+# f()
+# def repeat(n):
+#     def decorator(func):
+#         def wrapper(a):
+#             for i in range(n):
+#                 func(a)
+#         return wrapper
+#     return decorator
+
+# @repeat(3)
+# def greet(name):
+#     print(f"Hello, {name}!")
+
+# greet("world")
+def uppercase(func):
+    def wrapper():
+        return func().upper()
+    return wrapper
+def name(func):
+    def wrapper():
+        return "Hello "+func()
+    return wrapper
+
+@uppercase
+@name
+def abhi():
+    return "ABhi"
+
+print(abhi())
