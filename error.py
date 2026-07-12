c285760c07b57c196515d995b04da752a4614f29
Abhi Baweja <abaweja001@gmail.com>
2026-07-12 09:14:59 +0530

    error file added

---
+++ error.py
@@ -0,0 +1,9 @@
+for i in range(0,4):
+    try:
+        a = int(input("Enter Ist number: "))
+        b = int(input("Enter IInd number: "))
+
+        c = a/b
+        print(c)
+    except:
+        print("Some Error occured")
