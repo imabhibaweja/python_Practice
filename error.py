469420cb56669d98ab48989803367ba9b08164a9
Abhi Baweja <abaweja001@gmail.com>
2026-07-14 09:51:06 +0530

    multiple handling exceptions

--- error.py
+++ error.py
@@ -1,10 +1,33 @@
-for i in range(0,4):
-    try:
-        a = int(input("Enter Ist number: "))
-        b = int(input("Enter IInd number: "))
+# for i in range(0,4):
+#     try:
+#         a = int(input("Enter Ist number: "))
+#         b = int(input("Enter IInd number: "))
 
-        c = a/b
-        print(c)
-    except Exception as e:
-        print("Some Error occured")
-        print("Error is ", e)
+#         c = a/b
+#         print(c)
+#     except Exception as e:
+#         print("Some Error occured")
+#         print("Error is ", e)
+
+# try: 
+#     a =int(input("Enter Ist number "))
+#     b = int(input("Enter IInd number"))
+#     c = a/b
+#     print(c)
+
+# except ZeroDivisionError:
+#     print("Zero error")
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except ValueError:
+    print("Invalid input! Please enter a number.")
+
+# Alternative using a tuple:
+try:
+  num = int(input("Enter a number: "))
+  result = 10 / num
+except (ZeroDivisionError, ValueError) as e:
+  print(f"An error occurred: {e}")
